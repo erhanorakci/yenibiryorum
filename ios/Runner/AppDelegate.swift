238ce523fa26984ddbdccd7b402b7afe3be2b19b
erhanorakci <erhan.orakci@hotmail.com>
2025-12-02 23:24:13 +0300

    Update correct iOS Maps Key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,7 +9,7 @@ import GoogleMaps // 1. Bu satır eklendi
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     // 2. API Anahtarı buraya eklendi
-    GMSServices.provideAPIKey("AIzaSyCT18QMZpO-RohkM4hp1tbVhTCJl4j1g_U")
+    GMSServices.provideAPIKey("AIzaSyCr3d9xsVRaS_2njjo2ZG1dj0lKD94smUg")
     
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
